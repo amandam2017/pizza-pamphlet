215b487d96d9a8299b9fcae39d436112b069daeb
amandam2017 <amandamaarman19911@gmail.com>
2022-05-09 12:20:24 +0200

    updating the cost of the selected pizza size on button click

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,11 +1,28 @@
 // reference my elements
-// 1. all buttons
-// const orderMedBtnElem = document.querySelector('.buy-med');
-// const orderLargeBtnElem = document.querySelector('.buy-large');
 const orderSmallBtnElem = document.querySelector('.buy-small');
+const orderMediumBtnElem = document.querySelector('.buy-medium');
+const orderLargeBtnElem = document.querySelector('.buy-large');
+
 const cartElem = document.getElementById('hidden');
 
+
 orderSmallBtnElem.addEventListener('click', function(){
+    const order = cartElem.value;
+    if(order){
+        console.log(cartElem.value)
+    }
+    cartElem.style.display = 'block';
+})
+
+orderMediumBtnElem.addEventListener('click', function(){
+    const order = cartElem.value;
+    if(order){
+        console.log(cartElem.value)
+    }
+    cartElem.style.display = 'block';
+})
+
+orderLargeBtnElem.addEventListener('click', function(){
     const order = cartElem.value;
     if(order){
         console.log(cartElem.value)
