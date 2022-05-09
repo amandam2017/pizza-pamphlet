5eb56815fec148229f568a0f19e2b884bcba507f
amandam2017 <amandamaarman19911@gmail.com>
2022-05-09 13:01:35 +0200

    I have worked on grandtotal and it is now working. I have also separated the pizza x-data to app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,18 @@
-const { init } = require("express/lib/application")
 
 document.addEventListener('alpine:init', () => {
-    Alpine.data('qty', () => ({
-        increaseOrDecrease: 0,
-
-        init(){
-            this.increaseOrDecrease
+    Alpine.data('pizzas', () => ({
+        smallTotalPrice: 0.00, 
+        mediumTotalPrice: 0.00, 
+        largeTotalPrice: 0.00,
+        smallQty: 0, 
+        mediumQty: 0, 
+        largeQty: 0,
+        totalPrice() {
+            console.log(this.smallTotalPrice)
+            return this.smallTotalPrice + Number(this.mediumTotalPrice) + Number(this.largeTotalPrice)
+        },
+        addSmall(){
+            this.smallQty++
         }
-
     }))
 })
