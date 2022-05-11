70d10f09672e90c4fe9776d1bc479b827ac04155
amandam2017 <amandamaarman19911@gmail.com>
2022-05-11 11:34:00 +0200

    removed logic from index.html file to app.js file

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,8 @@
 
 document.addEventListener('alpine:init', () => {
     Alpine.data('pizzas', () => ({
-        smallTotalPrice: 0.00, show(){
+        smallTotalPrice: 0.00, 
+        show(){
             return this.totalPrice()>0;
         },
         mediumTotalPrice: 0.00, 
@@ -18,11 +19,46 @@ document.addEventListener('alpine:init', () => {
             return this.smallQty + this.mediumQty + this.largeQty
 
         },
-        addSmall(){
+        smallQtyUp(){
              this.smallQty++
-        }
-        // subtractSmall(){
-        //     this.smallQty
-        // }
+        },
+        smallQtyDown(){
+            this.smallQty--
+        },
+        smallPriceUp(){
+            this.smallTotalPrice+=48.99
+        },
+        smallPriceDown(){
+            this.smallTotalPrice-=48.99
+        },
+
+        //********** MEDIUM DATA *************
+        medQtyUp(){
+            this.mediumQty++
+       },
+       medQtyDown(){
+           this.mediumQty--
+       },
+       medPriceUp(){
+           this.mediumTotalPrice+=78.99
+       },
+       medPriceDown(){
+           this.mediumTotalPrice-=78.99
+       },
+
+    //*************LARGE DATA****************
+
+    largeQtyUp(){
+        this.largeQty++
+    },
+    largeQtyDown(){
+        this.largeQty--
+    },
+    largePriceUp(){
+        this.largeTotalPrice+=114.99
+    },
+    largePriceDown(){
+        this.largeTotalPrice-=114.99
+    }
     }))
 })
